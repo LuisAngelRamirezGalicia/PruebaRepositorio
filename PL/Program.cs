c0508e4a67c5e53dff3e6289c51cb99787e4157c
digis <digis@ALIEN22>
2023-12-12 18:36:28 -0600

    Add project files.

---
+++ PL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("primera version ");
+            Console.ReadKey();
+        }
+    }
+}
